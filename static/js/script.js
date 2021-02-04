c96e5d4646a69efe1554bacf1417f95fd4e59440
alychinque <alychinque@gmail.com>
2021-02-04 04:57:03 +0000

    Add collapsible function.

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,32 @@
+/**
+ * When the user pass the mouse over a card
+ * this function highlights the card
+ */
 function over(card){
     card.style.background = '#f1bc80'
-    }
-    function out(card){
+}
+/**
+ * This function returns the original color of the card
+ */
+function out(card){
     card.style.background = '#ffeedb'
+}
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+/**
+ * This function creates a collapsible effect in
+ * ingredients and steps 
+ */
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
     }
+  });
+}
